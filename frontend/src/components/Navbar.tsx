d5ce6fa1ff32b902bfefb6934b31eb4d60c81229
Sunit <sunit@petdogs.local>
2026-06-23 20:10:37 -0400

    Auto-commit: Claude edited files

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -256,6 +256,7 @@ export function Navbar() {
               <NavLink to="/grooming" className={navLinkClass}>Grooming</NavLink>
               <NavLink to="/dog-walking" className={navLinkClass}>Dog Walking</NavLink>
               <NavLink to="/pet-supplies" className={navLinkClass}>Pet Supplies</NavLink>
+              <NavLink to="/pet-pages" className={navLinkClass}>Dog Pages</NavLink>
             </div>
           </div>
         </nav>
